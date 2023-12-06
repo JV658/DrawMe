2431e30007d5fe43f3f46b79848ebcdaabc3fdac
JV658 <90655082+JV658@users.noreply.github.com>
2023-12-06 17:06:54 -0500

    initialization of DrawMe app completed

---
+++ DrawMe/Line.swift
@@ -0,0 +1,13 @@
+//
+//  Line.swift
+//  DrawMe
+//
+//  Created by Cambrian on 2023-12-06.
+//
+
+import Foundation
+
+struct Line{
+    var begin: CGPoint
+    var end: CGPoint
+}
---
+++ DrawMe/canvas.swift
@@ -0,0 +1,15 @@
+//
+//  canvas.swift
+//  DrawMe
+//
+//  Created by Cambrian on 2023-12-06.
+//
+
+import UIKit
+
+class Canvas: UIView {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touch was registered")
+    }
+}
